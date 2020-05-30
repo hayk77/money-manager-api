ddba718e6662a1dd1047f09ed275be8a18882f5e
hayk77 <hayksimonyan7@gmail.com>
2020-05-30 13:56:37 +0400

    connect api to database, create user model

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
-
 const app = express();
 
+const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`server started on ${PORT}`));
+connectDB();
+
+app.listen(PORT, () => console.log(`start server: ${PORT}`));

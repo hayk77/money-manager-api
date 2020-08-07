332ac69c3a131730352a9dc1518365716e344731
hayk77 <hayksimonyan7@gmail.com>
2020-08-07 10:49:09 +0400

    move checker to a seperate module, implement chekcer for all record controllers

--- controllers/records.js
+++ controllers/records.js
@@ -1,63 +1,16 @@
 const { validationResult } = require('express-validator');
-const mongoose = require('mongoose');
 
 const Account = require('../models/Account');
 const Category = require('../models/Category');
 const Record = require('../models/Record');
 const User = require('../models/User');
-
-const Checker = (() => {
-  console.log('checking for user');
-
-  const _userExists = async (userId) => {
-    if (mongoose.Types.ObjectId.isValid(userId) === false) {
-      return false;
-    } else {
-      const user = await User.findOne({ _id: userId });
-      if (!user) {
-        return false;
-      }
-      return true;
-    }
-  };
-
-  const _accountExists = async (accountId) => {
-    if (mongoose.Types.ObjectId.isValid(accountId) === false) {
-      return false;
-    } else {
-      const account = await Account.findOne({ _id: accountId });
-      if (!account) {
-        return false;
-      }
-      return true;
-    }
-  };
-
-  const _categoryExists = async (categoryId) => {
-    if (mongoose.Types.ObjectId.isValid(categoryId) === false) {
-      return false;
-    } else {
-      const category = await Category.findOne({ _id: categoryId });
-      if (!category) {
-        return false;
-      }
-      return true;
-    }
-  };
-
-  return {
-    userExists: _userExists,
-    accountExists: _accountExists,
-    categoryExistst: _categoryExists,
-  };
-})();
+const dbDocumentChecker = require('../helpers/db-document-checker');
 
 exports.getRecords = async (req, res) => {
   const userId = req.user.id;
 
   try {
-    const userExists = await Checker.userExists(userId);
-
+    const userExists = await dbDocumentChecker.userExists(userId);
     if (!userExists) {
       return res
         .status(400)
@@ -84,9 +37,9 @@ exports.postRecord = async (req, res) => {
   const userId = req.user.id;
 
   try {
-    const userExists = await Checker.userExists(userId);
-    const accountExists = await Checker.accountExists(accountId);
-    const categoryExistst = await Checker.categoryExistst(categoryId);
+    const userExists = await dbDocumentChecker.userExists(userId);
+    const accountExists = await dbDocumentChecker.accountExists(accountId);
+    const categoryExistst = await dbDocumentChecker.categoryExistst(categoryId);
 
     if (!userExists) {
       return res
@@ -138,32 +91,59 @@ exports.putRecord = async (req, res) => {
   const { recordId } = req.params;
 
   try {
-    const user = await User.findOne({ _id: userId });
-    if (!user) {
-      return res.status(400).json({
-        errors: [
-          { msg: 'Invalid credentials. Please logout and sign in again' },
-        ],
-      });
-    }
-    const account = await Account.findOne({ _id: accountId });
-    if (!account) {
+    const userExists = await dbDocumentChecker.userExists(userId);
+    const accountExists = await dbDocumentChecker.accountExists(accountId);
+    const categoryExistst = await dbDocumentChecker.categoryExistst(categoryId);
+    const recordExists = await dbDocumentChecker.recordExists(recordId);
+
+    if (!userExists) {
+      return res
+        .status(400)
+        .json({ errors: [{ msg: 'Invalid credentials.' }] });
+    } else if (!accountExists) {
       return res
         .status(400)
         .json({ errors: [{ msg: 'Account with that id does not exist' }] });
-    }
-    const category = await Category.findOne({ _id: categoryId });
-    if (!category) {
+    } else if (!categoryExistst) {
       return res
         .status(400)
         .json({ errors: [{ msg: 'Category with that id does not exist' }] });
-    }
-    const record = await Record.findOne({ _id: recordId });
-    if (!record) {
+    } else if (!recordExists) {
       return res
         .status(400)
         .json({ errors: [{ msg: 'Record with that id does not exist' }] });
     }
+
+    const user = await User.findOne({ _id: userId });
+    const account = await Account.findOne({ _id: accountId });
+    const category = await Category.findOne({ _id: categoryId });
+    const record = await Record.findOne({ _id: recordId });
+
+    // const user = await User.findOne({ _id: userId });
+    // if (!user) {
+    //   return res.status(400).json({
+    //     errors: [
+    //       { msg: 'Invalid credentials. Please logout and sign in again' },
+    //     ],
+    //   });
+    // }
+    // const account = await Account.findOne({ _id: accountId });
+    // if (!account) {
+    //   return res
+    //     .status(400)
+    //     .json({ errors: [{ msg: 'Account with that id does not exist' }] });
+    // }
+    // const category = await Category.findOne({ _id: categoryId });
+    // if (!category) {
+    //   return res
+    //     .status(400)
+    //     .json({ errors: [{ msg: 'Category with that id does not exist' }] });
+    // }
+    // if (!record) {
+    //   return res
+    //     .status(400)
+    //     .json({ errors: [{ msg: 'Record with that id does not exist' }] });
+    // }
 
     // update accounts total
     if (record.type === 'expences' && type === 'expences') {
@@ -201,43 +181,56 @@ exports.deleteRecord = async (req, res) => {
   const { recordId } = req.params;
 
   try {
-    const user = await User.findOne({ _id: userId });
-    if (!user) {
-      return res.status(400).json({
-        errors: [
-          { msg: 'Invalid credentials. Please logout and sign in again' },
-        ],
-      });
-    }
-    const record = await Record.findOne({ _id: recordId });
-    if (!record) {
+    const userExists = await dbDocumentChecker.userExists(userId);
+    const recordExists = await dbDocumentChecker.recordExists(recordId);
+
+    if (!userExists) {
+      return res
+        .status(400)
+        .json({ errors: [{ msg: 'Invalid credentials.' }] });
+    } else if (!recordExists) {
       return res
         .status(400)
         .json({ errors: [{ msg: 'Record with that id does not exist' }] });
     }
-    const account = await Account.findOne({ _id: record.account._id });
-    if (!account) {
-      return res
-        .status(400)
-        .json({ errors: [{ msg: 'Account with that id does not exist' }] });
-    }
 
-    console.log(account);
+    const user = await User.findOne({ _id: userId });
+    const record = await Record.findOne({ _id: recordId });
+    const account = await Account.findOne({ _id: record.account._id });
+
+    // const user = await User.findOne({ _id: userId });
+    // if (!user) {
+    //   return res.status(400).json({
+    //     errors: [
+    //       { msg: 'Invalid credentials. Please logout and sign in again' },
+    //     ],
+    //   });
+    // }
+    // const record = await Record.findOne({ _id: recordId });
+    // if (!record) {
+    //   return res
+    //     .status(400)
+    //     .json({ errors: [{ msg: 'Record with that id does not exist' }] });
+    // }
+    // const account = await Account.findOne({ _id: record.account._id });
+    // if (!account) {
+    //   return res
+    //     .status(400)
+    //     .json({ errors: [{ msg: 'Account with that id does not exist' }] });
+    // }
+
     if (record.type === 'expences') {
       account.total += record.amount;
     } else {
       account.total -= record.amount;
     }
     await account.save();
-    console.log(account);
 
     const indexOfRecord = user.records.indexOf(recordId);
     if (indexOfRecord !== -1) user.records.splice(indexOfRecord, 1);
     await user.save();
 
     await Record.findByIdAndRemove(recordId, { useFindAndModify: false });
-
-    console.log(user);
 
     res.status(201).json({ msg: 'Record was removed' });
   } catch (err) {
---
+++ helpers/db-document-checker.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+
+const Account = require('../models/Account');
+const Category = require('../models/Category');
+const Record = require('../models/Record');
+const User = require('../models/User');
+module.exports = (() => {
+  console.log('checking for user');
+
+  const _userExists = async (userId) => {
+    if (mongoose.Types.ObjectId.isValid(userId) === false) {
+      return false;
+    } else {
+      const user = await User.findOne({ _id: userId });
+      if (!user) {
+        return false;
+      }
+      return true;
+    }
+  };
+
+  const _accountExists = async (accountId) => {
+    if (mongoose.Types.ObjectId.isValid(accountId) === false) {
+      return false;
+    } else {
+      const account = await Account.findOne({ _id: accountId });
+      if (!account) {
+        return false;
+      }
+      return true;
+    }
+  };
+
+  const _categoryExists = async (categoryId) => {
+    if (mongoose.Types.ObjectId.isValid(categoryId) === false) {
+      return false;
+    } else {
+      const category = await Category.findOne({ _id: categoryId });
+      if (!category) {
+        return false;
+      }
+      return true;
+    }
+  };
+
+  const _recordExists = async (recordId) => {
+    if (mongoose.Types.ObjectId.isValid(recordId) === false) {
+      return false;
+    } else {
+      const record = await Record.findOne({ _id: recordId });
+      if (!record) {
+        return false;
+      }
+      return true;
+    }
+  };
+
+  return {
+    userExists: _userExists,
+    accountExists: _accountExists,
+    categoryExistst: _categoryExists,
+    recordExists: _recordExists,
+  };
+})();

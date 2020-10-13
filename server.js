fe4802c2189de5d3df5efb1d37f7a46f437db34b
Hayk <hsimonyan09@gmail.com>
2020-10-13 11:10:50 +0400

    allow CORS headers

--- server.js
+++ server.js
@@ -24,7 +24,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT,  DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-auth-token');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-auth-token, content-type');
+  res.setHeader("Content-Type", "application/json;charset=utf-8");
   next();
 });
     
